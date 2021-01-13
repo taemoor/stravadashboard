0a91a29d96e94d5142509969ffb453968703469b
tabbasi <taemoor.abbasi@nokia.com>
2021-01-12 23:08:02 -0500

    Add express route handling

--- index.js
+++ index.js
@@ -21,10 +21,17 @@ app.use(passport.session())
 
 require('./routes/authRoutes')(app)
 
-app.get('/', (req, res) => {
-    res.send({ Welcome: 'to Strava Dashboard Git 3' })
-  }
-)
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets.
+  // like our main.js file, or main.css file.
+  app.use(express.static('client/build'))
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
